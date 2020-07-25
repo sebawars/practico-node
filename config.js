42554a02ab2e27d468c06607f20862a7523973fc
sebawars <sebastianmoralesnap@gmail.com>
2020-07-24 22:14:55 -0300

    Local mysql

--- config.js
+++ config.js
@@ -10,22 +10,22 @@ module.exports = {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
     mysql: {
-        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'oqofkk3Wd9',
         password: process.env.MYSQL_PASS || '9un9x2szLp',
         database: process.env.MYSQL_DB || 'oqofkk3Wd9',
     },
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || 'localhost',
-        port: process.env.MYSQL_SRV_PORT || 3001,
+        port: process.env.MYSQL_SRV_PORT || 3005,
     },
     cacheService: {
         host: process.env.CACHE_SRV_HOST || 'localhost',
         port: process.env.CACHE_SRV_PORT || 3003,
     },
     redis: {
-        host: process.env.REDIS_HOST || "redis-13768.c10.us-east-1-3.ec2.cloud.redislabs.com",
-        port: process.env.REDIS_PORT || "13768",
-        password: process.env.REDIS_PASS || "x9iZI4GP656WDV5TvSKqXajnGhQfvWNO"
+        host: process.env.REDIS_HOST || "redis-14871.c11.us-east-1-3.ec2.cloud.redislabs.com",
+        port: process.env.REDIS_PORT || "14871",
+        password: process.env.REDIS_PASS || "9q6bAyOtCJYVXfH7S1ExCVdTqzJ50cgN"
     }
 }
