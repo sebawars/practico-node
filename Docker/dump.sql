965640f5ae19c766dc57df42c18384a78473f0c8
sebawars <sebastianmoralesnap@gmail.com>
2020-07-25 10:26:11 -0300

    Docker mysql

---
+++ Docker/dump.sql
@@ -0,0 +1,18 @@
+CREATE TABLE user(
+    id VARCHAR(32),
+    username VARCHAR(32),
+    name VARCHAR(64)
+);
+
+INSERT INTO `user`(`id`,`username`,`name`)
+VALUES ('123','cristian','cristian');
+
+CREATE TABLE auth(
+    id VARCHAR(32),
+    username VARCHAR(32),
+    password VARCHAR(64)
+);
+
+CREATE USER 'ec2-user'@'localhost' IDENTIFIED BY '9un9x2szLp';
+
+GRANT ALL ON oqofkk3Wd9.* TO 'ec2-user'@'localhost';
