5cf0fd06ef51caa5b7f7fd61b6b3376e41ce8050
sebawars <sebastianmoralesnap@gmail.com>
2020-07-25 16:16:30 -0300

    Fix password

--- Docker/db-dump/dump.sql
+++ Docker/db-dump/dump.sql
@@ -17,12 +17,15 @@ CREATE TABLE auth(
     password VARCHAR(64)
 );
 
-CREATE USER 'ec2-user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'ec2-user'@'localhost' IDENTIFIED BY 'ec2pass';
 
 GRANT ALL PRIVILEGES ON *.* TO 'ec2-user'@'localhost' WITH GRANT OPTION;
 
-CREATE USER 'ec2-user'@'%' IDENTIFIED BY 'password';
+CREATE USER 'ec2-user'@'%' IDENTIFIED BY 'ec2pass';
 
 GRANT ALL PRIVILEGES ON *.* TO 'ec2-user'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
+
+ALTER USER 'ec2-user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ec2pass';
+ALTER USER 'ec2-user'@'%' IDENTIFIED WITH mysql_native_password BY 'ec2pass';
